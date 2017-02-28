e9a457f7187a4d3c11b9b03191b9bca58dac9836
Sijawusz Pur Rahnama <sija@sija.pl>
2017-03-01 00:38:47 +0100

    Change Interface::HTTP to match protocol spec

--- src/raven/interfaces/http.cr
+++ src/raven/interfaces/http.cr
@@ -2,8 +2,10 @@ module Raven
   class Interface::HTTP < Interface
     property! url : String
     property! method : String
+    property query_string : String?
+    property cookies : String?
 
-    any_json_property :data, :query_string, :cookies, :headers, :env
+    any_json_property :env, :headers, :data
 
     def self.sentry_alias
       :request
