99deaeb85094c4899429c529a034fc2b6d92a2c9
Sijawusz Pur Rahnama <sija@sija.pl>
2017-03-02 07:46:57 +0100

    Simplify Raven::Logger

--- src/raven/logger.cr
+++ src/raven/logger.cr
@@ -2,18 +2,11 @@ require "logger"
 
 module Raven
   class Logger < ::Logger
-    LOG_PREFIX = "** [Raven] "
-    PROGNAME   = "sentry"
+    PROGNAME = "sentry"
 
     def self.new(*args, **options)
       super.tap do |logger|
         logger.progname = PROGNAME
-
-        original_formatter = logger.formatter
-        logger.formatter = Formatter.new do |severity, datetime, progname, message, io|
-          message = "#{LOG_PREFIX}#{message}"
-          original_formatter.call severity, datetime, progname, message, io
-        end
       end
     end
   end
