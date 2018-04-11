7206411649658586c24dee87592e61eb43407167
Sijawusz Pur Rahnama <sija@sija.pl>
2018-04-11 21:32:41 +0200

    Remove support for rather old Kemal versions

--- src/raven/integrations/kemal/exception_handler.cr
+++ src/raven/integrations/kemal/exception_handler.cr
@@ -28,10 +28,7 @@ module Raven
           lookup_result = context.route_lookup
           if lookup_result.found?
             lookup_result.payload.as(::Kemal::Route).tap do |route|
-              # https://github.com/kemalcr/kemal/pull/332
-              if route.responds_to?(:method) && route.responds_to?(:path)
-                event.culprit = "#{route.method} #{route.path}"
-              end
+              event.culprit = "#{route.method} #{route.path}"
             end
           end
           request = context.request
