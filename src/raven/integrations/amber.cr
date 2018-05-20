92be12e42c81d8c7f06c56ad012eb6fdc875d86d
Sijawusz Pur Rahnama <sija@sija.pl>
2018-05-21 01:13:17 +0200

    Add Ameba framework integration

---
+++ src/raven/integrations/amber.cr
@@ -0,0 +1,25 @@
+module Raven
+  # ```
+  # require "raven/integrations/amber"
+  # ```
+  #
+  # It's recommended to enable `Configuration#async` when using Amber.
+  #
+  # ```
+  # Raven.configure do |config|
+  #   # ...
+  #   config.async = true
+  #   config.current_environment = Amber.env.to_s
+  # end
+  # ```
+  module Amber
+    # Returns full URL string for `HTTP::Request`.
+    def self.build_request_url(req : HTTP::Request)
+      String.build do |url|
+        url << ::Amber::Server.instance.scheme << "://" << req.host_with_port << req.resource
+      end
+    end
+  end
+end
+
+require "./amber/**"
