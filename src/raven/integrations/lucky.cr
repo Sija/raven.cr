8e20683e156d4cd9453e7f0c23f7e3a8c491448b
Sijawusz Pur Rahnama <sija@sija.pl>
2018-09-20 03:11:27 +0200

    Add Lucky framework integration

---
+++ src/raven/integrations/lucky.cr
@@ -0,0 +1,33 @@
+require "uri"
+require "lucky"
+
+module Raven
+  # ```
+  # require "raven"
+  # require "raven/integrations/lucky"
+  # ```
+  #
+  # It's recommended to enable `Configuration#async` when using Lucky.
+  #
+  # ```
+  # Raven.configure do |config|
+  #   # ...
+  #   config.async = true
+  #   config.current_environment = Lucky::Env.name
+  # end
+  # ```
+  module Lucky
+    # Returns full URL string for `HTTP::Request`.
+    def self.build_request_url(req : HTTP::Request)
+      base_uri = URI.parse(::Lucky::RouteHelper.settings.base_uri)
+      String.build do |url|
+        # TODO: Use just `::Lucky::RouteHelper.settings.base_uri` if possible
+        url << base_uri.scheme << "://" << req.host_with_port << req.resource
+      end
+    end
+  end
+end
+
+Raven::Configuration::IGNORE_DEFAULT << Lucky::RouteNotFoundError
+
+require "./lucky/**"
