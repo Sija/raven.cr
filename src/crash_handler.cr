624befe92ba8d06c2ba838f60bc641c2d82e3b7e
Sijawusz Pur Rahnama <sija@sija.pl>
2020-01-20 01:10:49 +0100

    Simplify logic since we don’t monkey-patch Logger anymoar

--- src/crash_handler.cr
+++ src/crash_handler.cr
@@ -53,11 +53,7 @@ module Raven
     property logger : ::Logger {
       Logger.new({{ "STDOUT".id unless flag?(:release) }}).tap do |logger|
         logger.level = {{ flag?(:debug) ? "Logger::DEBUG".id : "Logger::ERROR".id }}
-
-        "#{logger.progname}.crash_handler".tap do |progname|
-          logger.progname = progname
-          configuration.exclude_loggers << progname
-        end
+        logger.progname = "raven.crash_handler"
       end
     }
 
