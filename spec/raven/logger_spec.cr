99601773bcdec02f44d433d0b8e315bd7a25cf7d
wout <wout@mick-wout.com>
2020-06-16 15:36:42 +0100

    Add spec for logger with IO backend.

--- spec/raven/logger_spec.cr
+++ spec/raven/logger_spec.cr
@@ -14,4 +14,16 @@ describe Raven::Logger do
     logs.check(:info, "Oh YAZ!")
     logs.next(:fatal, "Oh noes!")
   end
+
+  it "should log to a given IO" do
+    io = IO::Memory.new
+    backend = Log::IOBackend.new(io)
+    log = Raven::Logger.new(backend, :info)
+
+    log.info { "Oh YAZ!" }
+    log.fatal { "Oh noes!" }
+
+    io.to_s.should match(/INFO - sentry: Oh YAZ!\n/)
+    io.to_s.should match(/FATAL - sentry: Oh noes!\n\Z/)
+  end
 end
