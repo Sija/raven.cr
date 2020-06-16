ec2833ce6deb3355eeebb609dc77bb4723a39bea
wout <wout@mick-wout.com>
2020-06-16 15:19:34 +0100

    Rename logger spec.

--- spec/raven/logger_spec.cr
+++ spec/raven/logger_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 require "log/spec"
 
 describe Raven::Logger do
-  it "should log to a given IO" do
+  it "should log to a given backend" do
     backend = Log::MemoryBackend.new
     log = Raven::Logger.new(backend, :info)
 
