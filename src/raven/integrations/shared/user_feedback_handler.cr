f9aa11392c3fb664f47811294226152da3812762
Sijawusz Pur Rahnama <sija@sija.pl>
2020-06-24 15:45:56 +0200

    Skip printing feedback view for closed responses

--- src/raven/integrations/shared/user_feedback_handler.cr
+++ src/raven/integrations/shared/user_feedback_handler.cr
@@ -5,6 +5,12 @@ module Raven
     rescue ex
       raise ex unless Raven.configuration.capture_allowed?(ex)
       context.response.tap do |response|
+        if response.closed?
+          Log.warn {
+            "Couldn't render user feedback view because the response has already been closed"
+          }
+          next
+        end
         response.status_code = 500
         response.print render_view(ex)
       end
