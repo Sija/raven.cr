38df568725e3b2d6fa6a8660465c9e8465f067f5
Caspian Baska <caspianbaska@gmail.com>
2021-01-06 16:49:45 +1100

    Add support for Spider Gazelle's ActionContoller

---
+++ src/raven/integrations/action-controller.cr
@@ -0,0 +1,24 @@
+require "action-controller"
+
+module Raven
+  # ```
+  # require "raven"
+  # require "raven/integrations/action-controller"
+  # ```
+  #
+  # It's recommended to enable `Configuration#async` when using ActionController.
+  #
+  # ```
+  # Raven.configure do |config|
+  #   # ...
+  #   config.async = true
+  # end
+  # ```
+  module ActionController
+    def self.build_request_url(req : HTTP::Request)
+      "#{::ActionController::Support.request_protocol(req)}://#{req.host_with_port}#{req.resource}"
+    end
+  end
+end
+
+require "./action-controller/*"
---
+++ src/raven/integrations/action-controller/error_handler.cr
@@ -0,0 +1,44 @@
+require "http"
+require "../http/*"
+
+module Raven
+  module ActionController
+    # Exception handler capturing all unhandled `Exception`s.
+    # After capturing exception is re-raised.
+    #
+    # ```
+    # server = HTTP::Server.new([
+    #   # ...
+    #   ActionController::ErrorHandler.new(production: true),
+    #   Raven::ActionController::ErrorHandler.new,
+    #   # ...
+    # ])
+    # ```
+    class ErrorHandler
+      include HTTP::Handler
+      include Raven::HTTPHandler
+
+      # See `::HTTP::Request`
+      CULPRIT_PATTERN_KEYS = %i(method path)
+
+      def initialize(
+        @culprit_pattern = "%{method} %{path}",
+        @capture_data_for_methods = %w(POST PUT PATCH),
+        @default_logger = "action-controller"
+      )
+      end
+
+      def build_raven_culprit_context(context : HTTP::Server::Context)
+        context.request
+      end
+
+      def build_raven_http_url(context : HTTP::Server::Context)
+        ActionController.build_request_url(context.request)
+      end
+
+      def build_raven_http_data(context : HTTP::Server::Context)
+        ::ActionController::Base.extract_params(context).to_h
+      end
+    end
+  end
+end
