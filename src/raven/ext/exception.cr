1284b37b99a037eb2fdba0f6a2fe9f83ef7eadea
Stephen Dolan <dolan.stephen1@gmail.com>
2021-03-04 15:35:02 -0500

    [WIP] Add Serialization Ignores to Raven Exception
    Closes #82, which can be viewed for more detail

--- src/raven/ext/exception.cr
+++ src/raven/ext/exception.cr
@@ -1,7 +1,11 @@
 class Exception
+  @[JSON::Field(ignore: true)]
+  @[YAML::Field(ignore: true)]
   property __raven_event_id : String?
 
   {% for key in %i(user tags extra) %}
+    @[JSON::Field(ignore: true)]
+    @[YAML::Field(ignore: true)]
     any_json_property :__raven_{{ key.id }}
   {% end %}
 end
