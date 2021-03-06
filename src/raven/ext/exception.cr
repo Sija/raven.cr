d53319dc4ce26fc44f13b9c1c6ffb8699e22899b
Stephen Dolan <dolan.stephen1@gmail.com>
2021-03-06 15:55:16 -0500

    Add serialization ignores to custom properties within the Exception class (#83)
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/raven/ext/exception.cr
+++ src/raven/ext/exception.cr
@@ -1,7 +1,14 @@
+require "json"
+require "yaml"
+
 class Exception
+  @[JSON::Field(ignore: true)]
+  @[YAML::Field(ignore: true)]
   property __raven_event_id : String?
 
   {% for key in %i(user tags extra) %}
+    @[JSON::Field(ignore: true)]
+    @[YAML::Field(ignore: true)]
     any_json_property :__raven_{{ key.id }}
   {% end %}
 end
